f82785fc7370e7fed75f79b4ced9da6345140919
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-07-13 15:36:11 +0530

    feat: add prompts

--- readme.md
+++ readme.md
@@ -13,3 +13,7 @@ yarn dev
 
 # You will see the output being logged out in your terminal
 ```
+
+The [`utils.ts`](./src/utils.ts) file contains some helper functions for interacting with the Ankr Advanced APIs.
+
+The [`app.ts`](./src/app.ts) file calls those helper functions and logs out the output to your terminal.
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 import { getAccountBalance, getNFTs } from './utils';
+const prompts = require('prompts');
 
 const main = async () => {
+  const addressToQueryNFTsFor = await prompts({
+    type: 'text',
+    name: 'address',
+    message: 'Enter an address to query NFTs for',
+  });
   console.log('NFTS =================');
-  const nfts = await getNFTs('0x0ED6Cec17F860fb54E21D154b49DAEFd9Ca04106');
+  const nfts = await getNFTs(addressToQueryNFTsFor.address);
   console.log(nfts);
 
+  const addressToQueryBalancesFor = await prompts({
+    type: 'text',
+    name: 'address',
+    message: 'Enter an address to query token balances for',
+  });
   console.log('TOKEN BALANCES =================');
-  const balances = await getAccountBalance(
-    '0x0ED6Cec17F860fb54E21D154b49DAEFd9Ca04106'
-  );
+  const balances = await getAccountBalance(addressToQueryBalancesFor.address);
   console.log(balances);
 };
 
